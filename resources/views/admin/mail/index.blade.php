0db09314492d21016fdc335e4501b9c5be623b5d
nisarizqi <135234764+nisarizqi@users.noreply.github.com>
2023-07-07 23:54:06 +0700

    Add files via upload

---
+++ resources/views/admin/mail/index.blade.php
@@ -0,0 +1,15 @@
+Halo, {{ $name }}!
+<br>
+<br>
+Akun Anda berhasil dibuat!
+<br>
+Berikut adalah data Anda:
+<br>
+Username&nbsp;: {{ $username }}
+<br>
+Password&nbsp;: {{ $password }}
+<br>
+<br>
+<br>
+Silahkan login menggunakan detail akun di atas!
+Terima kasih :)
